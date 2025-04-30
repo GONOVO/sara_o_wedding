25eb845bf13c3b1d1c1e67036f1732dbd3c4b20e
alaagonovo <alaa@gonovo.tech>
2025-04-30 11:52:19 +0300

    end services page

--- src/components/ui/servicesection/Servicesection.tsx
+++ src/components/ui/servicesection/Servicesection.tsx
@@ -1,40 +1,48 @@
+import services from "@/data/services";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 
 function Servicesection() {
   return (
-    <section className="max-w-7xl mx-auto px-4 lg:my-16 my-8 grid grid-cols-1 lg:grid-cols-2 gap-8">
-      <div className="relative w-full h-[60dvh] sm:h-[100dvh] mx-auto mt-8 sm:mt-4">
-        <Image
-          src="/images/serv1.JPG"
-          alt="logo Image"
-          fill
-          className="object-cover"
-          // sizes="(max-width: 768px) 30vw, 50vw"
-        />
-      </div>
-      <div>
-        <h1 className="text-5xl lg:mt-28 lg:mb-10 my-10">Private Events</h1>
-        <ul>
-          <li>Milestone Birthdays</li>
-          <li>Private Celebrations Events (any space)</li>
-          <li>Weddings: Traditional & White/Civil Ceremonies & Receptions</li>
-          <li>Weddings: Vow ceremonies, Anniversaries & After Parties</li>
-          <li>
-            International Destination Celebrations (Birthdays, Weddings etc)
-          </li>
-          <li>Celebration of Life (Funeral) Receptions</li>
-          <li>Other Décor themed Concepts or Events</li>
-        </ul>
-        <Link
-          href="/portfolio/#gallery"
-          className="inline-block my-10   border-2 py-2 px-8 tracking-[4px] text-sm nunito_font hover:bg-black hover:text-white"
+    <>
+      {services.map((service, index) => (
+        <section
+          key={index}
+          className={`max-w-7xl mx-auto px-4 lg:my-16 my-8 grid grid-cols-1 ${
+            index % 2 === 0 ? "lg:grid-cols-2" : "lg:grid-cols-[1fr_2fr]"
+          } gap-8`}
         >
-          VIEW GALLERY
-        </Link>
-      </div>
-    </section>
+          <div
+            className={`relative w-full h-[60dvh] sm:h-[100dvh] mx-auto  ${
+              index % 2 === 0 ? "order-1" : "order-2"
+            }`}
+          >
+            <Image
+              src={service.image}
+              alt="logo Image"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className={index % 2 === 0 ? "order-2" : "order-1"}>
+            <h1 className="text-5xl lg:mt-28 lg:mb-10 my-10">
+              {service.title}
+            </h1>
+            <ul className="list-disc ms-8">
+              {service.services.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+            <Link
+              href="/portfolio/#gallery"
+              className="inline-block my-10   border-2 py-2 px-8 tracking-[4px] text-sm nunito_font hover:bg-black hover:text-white"
+            >
+              VIEW GALLERY
+            </Link>
+          </div>
+        </section>
+      ))}
+    </>
   );
 }
 
---
+++ src/data/services.ts
@@ -0,0 +1,30 @@
+const services = [
+  {
+    image: "/images/serv1.JPG",
+    title: "Private Events",
+    services: [
+      "Milestone Birthdays.",
+      "Private Celebrations Events (any space).",
+      "Weddings: Traditional & White/Civil Ceremonies & Receptions.",
+      "Weddings: Vow ceremonies, Anniversaries & After Parties.",
+      "International Destination Celebrations (Birthdays, Weddings etc).",
+      "Celebration of Life (Funeral) Receptions",
+      "Other Décor themed Concepts or Events",
+    ],
+  },
+  {
+    image: "/images/serv2.JPG",
+    title: "Corporate Events",
+    services: [
+      "Corporate Dinners & Gala Nights.",
+      "End of Year Celebrations & Themed Parties.",
+      "Summits/Forums & Conference Branded Events.",
+      "Milestone Popup Events & Product Activations.",
+      "Set Stage Designs & Bespoke Interior Space Decor.",
+      "Fashion & Entertainment Creative Industry Events.",
+      "Full scale Concept Event productions with partners.",
+    ],
+  },
+];
+
+export default services;

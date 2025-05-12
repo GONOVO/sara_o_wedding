37debe4da715211f8e20aa743f6556a7878e04e3
alaagonovo <alaa@gonovo.tech>
2025-05-12 16:00:52 +0300

    adding poster

--- src/data/eventsgallery.ts
+++ src/data/eventsgallery.ts
@@ -1,10 +1,10 @@
 const eventsImages = [
   "/images/ev4.webp",
   "/images/schweppes_event.webp",
-  "/images/portfolio2.webp",
-  "/images/portfolio3.JPEG",
-  "/images/ev3.webp",
   "/images/ev5.webp",
+  "/images/portfolio2.webp",
+  // "/images/ev3.webp",
+  "/images/portfolio3.JPEG",
   "/images/ev1.webp",
   "/images/ev2.webp",
 ];

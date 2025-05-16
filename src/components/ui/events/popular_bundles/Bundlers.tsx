dbf70b918fa18e96c587223904292ebf3a808777
alaagonovo <alaa@gonovo.tech>
2025-05-16 12:43:34 +0300

    update color of bunddles

--- src/components/ui/events/popular_bundles/Bundlers.tsx
+++ src/components/ui/events/popular_bundles/Bundlers.tsx
@@ -6,19 +6,16 @@ function Bundlers() {
   return (
     <section className="mt-36">
       <div className="w-full flex flex-col justify-center items-center">
-        <div
-          className="w-full h-[1px] bg-[var(--gold-color)]"
-          data-aos="fade-up"
-        ></div>
+        <div className="w-full h-[1px] bg-black" data-aos="fade-up"></div>
         <h1
-          className="italic text-4xl text-[var(--gold-color)] max-w-[250px] text-center -mt-14"
+          className="italic text-4xl max-w-[250px] text-center -mt-14"
           data-aos="fade-up"
         >
           <span className="not-italic me-2 font-medium text-[45px]">most</span>
           Popular <span className="bg-white my-2 px-8">bundles</span>
         </h1>
         <p
-          className="text-xl max-w-[820px] text-center mx-auto px-2 my-10 text-[var(--gold-color)]"
+          className="text-xl max-w-[820px] text-center mx-auto px-2 my-10"
           data-aos="fade-up"
           data-aos-delay={200}
         >

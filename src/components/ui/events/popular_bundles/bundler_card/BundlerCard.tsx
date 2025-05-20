1376081f7d9f2285c215018f5fbdbe1368f68c35
alaagonovo <alaa@gonovo.tech>
2025-05-20 17:00:18 +0300

    some updates in animation and data of portfolio

--- src/components/ui/events/popular_bundles/bundler_card/BundlerCard.tsx
+++ src/components/ui/events/popular_bundles/bundler_card/BundlerCard.tsx
@@ -1,25 +1,49 @@
 "use client";
 import { IBundle } from "@/utils/interfaces";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function BundlerCard({ bundle, index }: { bundle: IBundle; index: number }) {
   const [isLargeScreen, setIsLargeScreen] = useState(false);
-
+  const [isInView, setIsInView] = useState(false);
+  const cardRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     if (typeof window !== "undefined") {
       setIsLargeScreen(window.innerWidth >= 1024);
     }
   }, []);
-
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setTimeout(() => {
+            setIsInView(true);
+          }, index * 1000);
+        }
+      },
+      { threshold: 0.3 }
+    );
+    if (cardRef.current) observer.observe(cardRef.current);
+    return () => {
+      if (cardRef.current) observer.unobserve(cardRef.current);
+    };
+  }, [index]);
   return (
     <article
-      className="h-[600px] w-full sm:w-[330px] p-4 ps-8 bg-[var(--gold-color)] flex flex-col  transition-all duration-300 transform hover:-translate-y-3 hover:shadow-xl hover:bg-gray-200"
+      ref={cardRef}
+      // className="h-[600px] w-full sm:w-[330px] p-4 ps-8 bg-[var(--gold-color)] flex flex-col  transition-all duration-300 transform hover:-translate-y-3 hover:shadow-xl hover:bg-gray-200"
+      className={`h-[600px] w-full sm:w-[330px] p-4 ps-8 bg-[var(--gold-color)] flex flex-col 
+        transition-all duration-500 transform
+        ${
+          isInView
+            ? "opacity-100 -translate-y-3 bg-gray-200 shadow-xl"
+            : "translate-y-0"
+        }`}
       style={{
         marginTop: isLargeScreen ? `${index * 56}px` : "24px",
       }}
-      data-aos="zoom-in-out"
-      data-aos-delay={index * 150}
+      // data-aos="zoom-in-out"
+      // data-aos-delay={index * 150}
     >
       <div className="flex items-center gap-4 my-4">
         <span className="text-sm">The</span>
